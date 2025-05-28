a0a0905f50019431769eec0ddcd05e721f144759
Hussam.Alghamdi <Hussam.Alghamdi@trendyol.com>
2025-05-28 09:52:33 +0300

    guess number game

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+let myNumber = Math.trunc(Math.random() * 20) + 1;
+// let myNumber = 5;
+
+function check() {
+  let score = Number(document.querySelector('.score').textContent);
+  let guess = document.querySelector('.guess').value;
+  if (guess == myNumber) {
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('.highscore').textContent = score;
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.message').textContent = 'click again to retry';
+    document.body.style.backgroundColor = '#60b347';
+    myNumber = Math.trunc(Math.random() * 20) + 1;
+  } else if (guess < myNumber) {
+    document.querySelector('.message').textContent = 'Low';
+    document.querySelector('.score').textContent = score - 1;
+  } else if (guess > myNumber) {
+    document.querySelector('.message').textContent = 'Hihg';
+    document.querySelector('.score').textContent = score - 1;
+  }
+}
+function again() {
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.body.style.backgroundColor = '#222';
+}
+
+document.querySelector('.check').addEventListener('click', check);
+document.querySelector('.again').addEventListener('click', again);
