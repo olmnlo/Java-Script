5331982cb06338c99890b4d67beab0aa37fbc9b9
Hussam.Alghamdi <Hussam.Alghamdi@trendyol.com>
2025-05-28 10:28:59 +0300

    modal

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+function show() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+function close() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', show);
+}
+
+btnCloseModal.addEventListener('click', close);
+overlay.addEventListener('click', close);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      close();
+    }
+  }
+});
